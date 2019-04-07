2d255c41d41aeaa660f360d7f3d35d57a65ad913
danielxie1990 <xieyongchun151@gmail.com>
2019-04-07 10:22:39 +0800

    Move static html file folder to theme folder, convert index.html, updating the header, footer and body area of the index.php

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 	
 function university_files() {
 	wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+	wp_enqueue_style('university_font_awesome_styles', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 }
 
 add_action('wp_enqueue_scripts', 'university_files');

3bb1804b8daeb280aa2ffc336e4a1f9641a4417c
danielxie1990 <xieyongchun151@gmail.com>
2019-04-08 23:53:41 +0800

    use a variable to reduce duplicated code

--- page.php
+++ page.php
@@ -20,10 +20,12 @@
 
 	  	<?php 
 
-	  		if (wp_get_post_parent_id(get_the_ID())) { ?>
+	  		$theParent = wp_get_post_parent_id(get_the_ID());
+
+	  		if ($theParent) { ?>
 
  			<div class="metabox metabox--position-up metabox--with-home-link">
-	     	 	<p><a class="metabox__blog-home-link" href="<?php echo get_permalink(wp_get_post_parent_id(get_the_ID())); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title(wp_get_post_parent_id(get_the_ID())); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+	     	 	<p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
 	    	</div>
 
 	  			

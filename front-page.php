e91bafbd40c72a56e48f98177e7155ed27c98e87
danielxie1990 <xieyongchun151@gmail.com>
2019-04-11 10:39:55 +0800

    adjust the default query via hook 'pre_get_posts'

--- front-page.php
+++ front-page.php
@@ -26,7 +26,7 @@
           $homepageEvents = new WP_Query(array(
 
               'post_type' => 'event',
-              'posts_per_page' => -1,
+              'posts_per_page' => 2,
               'meta_key' => 'event_date',
               'orderby' => 'meta_value_num' ,
               'order' => 'ASC',

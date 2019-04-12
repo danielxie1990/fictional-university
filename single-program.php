1856dee4701be0fcaf95bf62ba30296d27af2212
danielxie1990 <xieyongchun151@gmail.com>
2019-04-12 23:44:48 +0800

    add a parameter to the_post_thumbnail('professorPortrait') the_post_thumbnail_url('professorLandscape') to get the idea size of featured images

--- single-program.php
+++ single-program.php
@@ -74,7 +74,7 @@ $relatedProfessors = new WP_Query(array(
 
     <li class="professor-card__list-item">
         <a class="professor-card" href="<?php the_permalink(); ?>">
-            <img src="<?php the_post_thumbnail_url();?>" class="professor-card__image">
+            <img src="<?php the_post_thumbnail_url('professorLandscape');?>" class="professor-card__image">
             <span class="professor-card__name"><?php the_title(); ?></span>
         </a>
     </li>

c8295c4236ec49e5f308b7d37ad360c36dd18611
danielxie1990 <xieyongchun151@gmail.com>
2019-04-13 23:50:57 +0800

    use the pageBanner function

--- single-professor.php
+++ single-professor.php
@@ -3,28 +3,11 @@
 	// keep looping while we still have post
 	while (have_posts()) {
 		// Function the_post() checks whether the loop has started and then sets the current post by moving, each time, to the next post in the queue.
-		the_post(); 
-
-		
-
+		the_post(); 		
+		pageBanner();
 		?>
 
-<div class="page-banner">
-    <div class="page-banner__bg-image"
-        style="background-image: url(<?php $pageBannerImage = get_field('page_banner_background_image');echo $pageBannerImage['sizes']['pageBanner']; ?>);">
-
-    </div>
-    <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
-        <div class="page-banner__intro">
-            <p><?php the_field('page_banner_subtitle'); ?></p>
-        </div>
-    </div>
-</div>
-
 <div class="container container--narrow page-section">
-
-
 
     <div class="generic-content">
         <div class="row group">
